ad3c2b7786e9c1a9334a358085407c953453f01d
Gerardo Galindez <ggalindezb@outlook.com>
2022-08-20 10:14:23 -0500

    Wire real login

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
 import React, { useState, useEffect } from 'react'
-import UserList from './components/UserList'
-import Appointments from './components/Appointments'
 import './App.css'
 
 function App() {
   const [jwt, setJwt] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-  useEffect(() => {
+  const handleEmailChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const handlePasswordChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const handleLogin = (event: React.FormEvent<HTMLInputElement>) => {
+    event.preventDefault();
+
     const request = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         auth: {
-          email: 'test@example.com',
-          password: 'test'
+          email: email,
+          password: password
         }
       })
     }
 
     fetch('http://localhost:3000/authentication/login', request)
-      .then(response => response.json())
-      .then(data => setJwt(data.access_token.jwt));
+      .then(async response => {
+        const payload = await response.json();
 
-    console.log(jwt);
-  })
+        if(!response.ok)
+          return Promise.reject(payload.errors);
+
+        return payload;
+      })
+      .then(data => {
+        setJwt(data.access_token.jwt);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
   return (
     <div className="App">
-      <UserList />
-      <Appointments />
+      <input type="text"
+        onChange={handleEmailChange} />
+      <input type="password"
+        onChange={handlePasswordChange} />
+      <input type="submit"
+        onClick={handleLogin}/>
+
+      JWT: {jwt}
     </div>
   )
 }

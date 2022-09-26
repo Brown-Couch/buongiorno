d2c31ff361b9229f6273aab333492b1c7bef6713
Gerardo Galindez <ggalindezb@outlook.com>
2022-09-25 19:31:58 -0500

    Add basic Students route, add search box

--- src/routes/students.tsx
+++ src/routes/students.tsx
@@ -1,3 +1,16 @@
+import './students.css'
+
 export default function Students() {
-  return <>Hello Students</>
+  return (
+    <div className="dashboard-card">
+      <div className="card-header">
+        <h1>Users</h1>
+        <p>30 users</p>
+      </div>
+      <div className="search-box">
+        <i className="fa-solid fa-magnifying-glass"></i>
+        <input type="text" placeholder="Search by name" />
+      </div>
+    </div>
+  )
 }
